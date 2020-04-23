c344cc68ab18b197e9e5b24695db9b43ee48135f
Hanisha <hanisha.vuppala@gmail.com>
2020-04-23 17:58:25 -0500

    initial project structure and other configuration files

---
+++ banking/src/main/java/com/ucm/controller/BankingController.java
@@ -0,0 +1,23 @@
+package com.ucm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class BankingController
+{
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "welcome";
+    }
+}
